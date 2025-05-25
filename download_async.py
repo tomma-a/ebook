7d472c9f7c984f217671a490bc2028ed1a3ab2eb
tomma <32663244+tomma-a@users.noreply.github.com>
2025-05-25 09:25:32 +0800

    Update download_async.py

--- download_async.py
+++ download_async.py
@@ -5,33 +5,44 @@ import asyncio
 import mechanicalsoup
 from bs4 import  BeautifulSoup
 import aiohttp
-urlindex="https://s.bailushuyuan.org/%E9%9D%92%E9%93%9C%E8%91%B5%E8%8A%B1"
-remove_tags=["script","meta","img","footer","link"]
-urls_selector="ol.text-body-tertiary li a"
-title="aa"
 async def fetch(session, url):
-    async with session.get(url) as response:
-        return await response.text()
+    retry=3;
+    while retry>0:
+        async with session.get(url) as response:
+          print(response.status)
+          if response.status==200:
+              return await response.text()
+          else:
+              retry=retry-1
+    return ""
 
+async def get_main_content(content):
+       return content.select("article.card")[0];
 async def parse(html):
     soup = BeautifulSoup(html, 'html.parser')
     # Do something with the links
     return soup
 async def clear(page,tags):
         for tag in tags:
-                all=browser.page.find_all(tag)
+                all=page.find_all(tag)
                 for  i in all:
                         i.decompose()
+        return True
 async def save(page,filename):
     with open(filename,"wb") as f:
         f.write(bytearray("<html><body>".encode("ascii")));
         f.write(bytearray(page.encode("ascii")))
         f.write(bytearray("</body></html>".encode("ascii")))
-
+    return True
 async def main():
+    urlindex="https://s.bailushuyuan.org/%E9%9D%92%E9%93%9C%E8%91%B5%E8%8A%B1"
+    baseurl="https://s.bailushuyuan.org"
+    remove_tags=["script","meta","img","footer","link"]
+    urls_selector="ol.text-body-tertiary li a"
+    title="aa"
     browser=mechanicalsoup.StatefulBrowser();
     browser.open(urlindex)
-    mainpage=browser.page;
+    mainpage=browser.page
     if title=="":
         title=mainpage.title.text;
     if not os.path.exists(title):
@@ -39,24 +50,23 @@ async def main():
     urls=mainpage.select(urls_selector)
     clear(mainpage,remove_tags)
     i=0;
-    for url in urls:
-        href=os.path.basename(url["href"])
-        if os.path.exists(os.path.join(title,href)):
-            continue;
-        retry=3;
-        while True:
-            r=browser.follow_link(url)
-            print(r.status_code)
-            if r.status_code==200:
-                break;
-            time.sleep(1)
-            retry=retry-1
-    time.sleep(0.06)
-    clear(browser.page,remove_tags+["a"])
-    maincontent=browser.page.select("article.card")[0];
-    save(maincontent,os.path.join(title,href+".html"))
+    async with aiohttp.ClientSession() as session:
+        for url in urls:
+            href=os.path.basename(url["href"])
+            if os.path.exists(os.path.join(title,href)):
+                continue;
+            print(baseurl+"/"+href)
+            content=await fetch(session,baseurl+"/"+url["href"])
+            if len(content)>0:
+                soup_content=await parse(content)
+                b=await clear(soup_content,remove_tags+["a"])
+                clear_c=await get_main_content(soup_content)
+                b=await save(clear_c,os.path.join(title,href+".html"))
     i+=1
     url["href"]=href+".html"
     #if i>5:
     #    break
-    save(mainpage,"index.html")
+    b=await save(mainpage,titile+"/index.html")
+
+
+asyncio.run(main())
